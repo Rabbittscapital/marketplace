23a41e5b859401c2d07c3904e1534cdcf6393531
Rabbittscapital <lmconejeros@gmail.com>
2025-09-21 01:01:03 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-export { auth as middleware } from "next-auth";
+// middleware.ts (v4)
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/dashboard/:path*", "/proyectos/:path*", "/unidad/:path*"]
