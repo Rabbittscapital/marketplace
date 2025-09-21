bf26754341d818f4d10de27d1fdf5e527d02654f
Rabbittscapital <lmconejeros@gmail.com>
2025-09-21 00:48:27 -0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-const g = global as any;
-export const prisma: PrismaClient = g.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") g.prisma = prisma;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
