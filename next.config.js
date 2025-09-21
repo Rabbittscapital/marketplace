b48130e0bcf63bc927ec62971f3b087ef4959578
Rabbittscapital <lmconejeros@gmail.com>
2025-09-20 23:50:42 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  experimental: {
+    serverActions: { allowedOrigins: ['*'] }
+  }
+};
 export default nextConfig;
