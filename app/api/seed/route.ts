2d2be21db53e2edabfefbdff1c7c7fa7faaaf606
Rabbittscapital <lmconejeros@gmail.com>
2025-09-21 01:19:15 -0300

    Update route.ts

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { hash } from "bcryptjs";
+import { hashSync } from "bcryptjs";
 
 export async function POST() {
   try {
